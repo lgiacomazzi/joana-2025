7d78c21ab77d2624053795b79dfcce342890f30d
Luciano <lucianogiacomazzi@gmail.com>
2024-09-08 18:48:03 -0300

    added segmented art fetches

--- app/[category]/[year]/page.tsx
+++ app/[category]/[year]/page.tsx
@@ -1,3 +1,34 @@
-export default function YearlyCategoryPage() {
-  return <p>Year</p>;
+import { fetchYearlyPaintings } from "@/lib/data";
+import { Art } from "@/lib/definitions";
+import Image from "next/image";
+
+type YearlyCategoryPageParams = {
+  year: string;
+  category: string;
+};
+
+export default async function YearlyCategoryPage({
+  params,
+}: {
+  params: YearlyCategoryPageParams;
+}) {
+  const arts = await fetchYearlyPaintings(params.year, params.category);
+
+  return (
+    <div>
+      <p>
+        {params.category} - {params.year}
+      </p>
+      {arts.map((art: Art, index: number) => (
+        <Image
+          key={index}
+          src={art.image_url}
+          alt={art.title}
+          priority={index === 0}
+          width={100}
+          height={100}
+        />
+      ))}
+    </div>
+  );
 }
--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,3 +1,42 @@
-export default function CategoryPage() {
-  return <p>Caterory</p>;
+import { fetchCategoryArts } from "@/lib/data";
+import { Art } from "@/lib/definitions";
+import { categoryTranslations } from "@/lib/utils";
+import { Metadata, ResolvingMetadata } from "next";
+import Image from "next/image";
+
+type CategoryPageParams = {
+  category: string;
+};
+
+export async function generateMetadata(
+  { params }: { params: CategoryPageParams },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const parentTitle = (await parent).title?.absolute;
+  const title = `${categoryTranslations[params.category]} - ${parentTitle}`;
+  return { title };
+}
+
+export default async function CategoryPage({
+  params,
+}: {
+  params: CategoryPageParams;
+}) {
+  const arts = await fetchCategoryArts(params.category);
+
+  return (
+    <div>
+      <p>{params.category}</p>
+      {arts.map((art: Art, index: number) => (
+        <Image
+          key={index}
+          src={art.image_url}
+          alt={art.title}
+          priority={index === 0}
+          width={100}
+          height={100}
+        />
+      ))}
+    </div>
+  );
 }
