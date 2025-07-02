ba34001d19e0f1d9596842f4fadc798f6d8cf094
Luciano <lucianogiacomazzi@gmail.com>
2025-07-02 16:11:19 -0300

    optimize load-more

--- components/gallery/load-more.tsx
+++ components/gallery/load-more.tsx
@@ -1,44 +1,64 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
 import { GetPaginatedArts } from "@/app/actions";
 import { Art } from "@/lib/definitions";
-import { useEffect, useState } from "react";
+
 import Spinner from "@/public/spinner.svg";
-import { useInView } from "react-intersection-observer";
 import Galery from "./galery";
 
-let page = 1;
-
 export default function LoadMore() {
-  const { ref, inView } = useInView();
-  const [data, setData] = useState<Art[]>([]);
-  const [loadMore, setLoadMore] = useState(true);
+  const { ref: loaderRef, inView } = useInView();
+  const [arts, setArts] = useState<Art[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const pageRef = useRef(1);
+
+  const fetchNextPage = async () => {
+    if (isLoading || !hasMore) return;
+
+    setIsLoading(true);
+    const nextPage = pageRef.current;
+
+    try {
+      const newArts = await GetPaginatedArts(nextPage);
+
+      if (Array.isArray(newArts) && newArts.length > 0) {
+        setArts((prev) => [...prev, ...newArts]);
+        pageRef.current += 1;
+      } else {
+        setHasMore(false);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     if (inView) {
-      console.log("# Loading more...");
-      GetPaginatedArts(page).then((data) => {
-        console.log(data);
-        if (Array.isArray(data) && data.length > 0) {
-          setData((prev) => [...prev, ...data]);
-        } else {
-          setLoadMore(false);
-        }
-      });
-      page++;
+      fetchNextPage();
+      // GetPaginatedArts(page).then((newArts) => {
+      //   if (Array.isArray(newArts) && newArts.length > 0) {
+      //     setArts((prev) => [...prev, ...newArts]);
+      //   } else {
+      //     setHasMore(false);
+      //   }
+      // });
+      // page++;
     }
-  }, [data, inView]);
+  }, [inView]);
 
   return (
     <>
-      <Galery arts={data} />
-
-      {loadMore && (
+      <Galery arts={arts} />
+      {hasMore && (
         <div
-          ref={ref}
+          ref={loaderRef}
           className="w-full flex justify-center flex-col items-center"
         >
-          <Spinner className="text-[--foreground-secondary]" />
+          {isLoading && <Spinner className="text-[--foreground-secondary]" />}
         </div>
       )}
     </>
